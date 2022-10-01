81468b2b3c4c60bfddaba57d5043df16e9f29016
oksana-1 <4438488@gmail.com>
2022-10-01 16:57:25 +0300

    Implement `undo`/`redo` functions with pinia $subscribe

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,35 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
 import { useCartStore } from "@/stores/CartStore";
+import { ref, reactive } from "vue";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
-cartStore.$onAction(({ name, store, args, after, onError}) => {
+const history = reactive([]);
+const future = reactive([]);
+const doingHistory = ref(false);
+history.push(JSON.stringify(cartStore.$state));
+const undo = () => {
+  if (history.length === 1) return;
+  doingHistory.value = true;
+  future.push(history.pop());
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+}
+const redo = () => {
+  const lastState =  future.pop();
+  if (!lastState) return;
+  doingHistory.value = true;
+  history.push(lastState);
+  cartStore.$state = JSON.parse(lastState);
+  doingHistory.value = false;
+}
+cartStore.$subscribe((mutation, state) => {
+  if (doingHistory.value === true) return;
+  history.push(JSON.stringify(state));
+  future.splice(0, future.length);
+});
+cartStore.$onAction(({ name, store, args, after, onError }) => {
   if (name === "addItems") {
     after(() => {
       console.log(`User added ${args[0]} "${args[1].name}"`);
@@ -19,6 +44,10 @@ productStore.fill();
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-10 text-center">
+      <AppButton class="mr-5" @click="undo">Undo</AppButton>
+      <AppButton @click="redo">Redo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
