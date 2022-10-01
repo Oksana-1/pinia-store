aa3d519ea275f197ea65603d07166d9ccea91d48
oksana-1 <4438488@gmail.com>
2022-10-01 15:00:04 +0300

    Use pinia in options API - getter as an object

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,7 +7,9 @@ import { mapState } from "pinia";
 export default {
   components: { CartWidget },
   computed: {
-    ...mapState(useAuthStore, ["userName"]),
+    ...mapState(useAuthStore, {
+      user: "userName",
+    }),
   },
 };
 </script>
@@ -19,7 +21,7 @@ export default {
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
     <div>
-      <span class="mr-5">{{ userName }}</span>
+      <span class="mr-5">{{ user }}</span>
       <CartWidget class="inline-block" />
     </div>
   </header>
