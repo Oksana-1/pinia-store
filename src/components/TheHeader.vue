5aae05465f08f64aaf67323f50f8495933f32a1a
oksana-1 <4438488@gmail.com>
2022-10-01 15:04:15 +0300

    Use pinia in options API - use mapWritableState

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,12 +2,12 @@
 // imports
 import CartWidget from "./CartWidget.vue";
 import { useAuthStore } from "@/stores/AuthStore";
-import { mapState } from "pinia";
+import { mapWritableState } from "pinia";
 
 export default {
   components: { CartWidget },
   computed: {
-    ...mapState(useAuthStore, {
+    ...mapWritableState(useAuthStore, {
       user: "userName",
     }),
   },
@@ -21,6 +21,7 @@ export default {
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
     <div>
+      <input v-model="user" type="text" class="mr-5">
       <span class="mr-5">{{ user }}</span>
       <CartWidget class="inline-block" />
     </div>
